7320c41193fe04a241ddd61da45f4eb003908408
José Pedro Pereira <ze.pereira96@gmail.com>
2022-02-17 21:15:45 +0000

    nav added to layout

--- models/Sneaker.model.js
+++ models/Sneaker.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
+const sneakerSchema = new Schema(
   {
     email: {
       type: String,
@@ -21,6 +21,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = model('User', userSchema);
+const Sneaker = model('Sneaker', sneakerSchema);
 
-module.exports = User;
+module.exports = Sneaker;
