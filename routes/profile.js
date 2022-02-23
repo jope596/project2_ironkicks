813e6c4a8276f972a5befb27b492b774dda8860e
José Pedro Pereira <ze.pereira96@gmail.com>
2022-02-23 18:11:54 +0000

    nav bar correct

--- routes/profile.js
+++ routes/profile.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const Sneaker = require("../models/Sneaker.model");
 const User = require("../models/User.model");
 
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 
 
-router.get("/profile", async (req, res, next) => {
-  User.findById(req.session.user._id)
+
+router.get("/profile", isLoggedIn, async (req, res, next) => {
+  await User.findById(req.session.user._id)
   .populate('favourite')
   .then((user) => {
     res.render("auth/profile", { user });
   })
-  
-  
 });
 
 // router.get("/profile", (req, res, next) => {
--- routes/sneakers.js
+++ routes/sneakers.js
@@ -5,6 +5,7 @@ const sneaks = new SneaksAPI();
 
 
 router.get("/sneakers", async(req, res, next) => {
+  
 
  await sneaks.getProducts("", 24, function(err, products){
   if (err) {
@@ -12,6 +13,9 @@ router.get("/sneakers", async(req, res, next) => {
     //return "No Products In Database";
   } else {
     console.log('products.data', products)
+    if(req.session.user) {
+      res.render("sneakers", {user: req.session.user, products});
+    }
     res.render('sneakers',{products});
   }
   })
